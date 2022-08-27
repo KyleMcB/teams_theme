fe95aaf8258551de995835ad90ae870d5d27e93d
GitHub Action <action@github.com>
2022-08-27 16:40:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/kylemcb/teamstheme/MyBundle.kt
+++ src/main/kotlin/com/github/kylemcb/teamstheme/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.kylemcb.teamstheme
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/kylemcb/teamstheme/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kylemcb/teamstheme/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kylemcb.teamstheme.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kylemcb.teamstheme.MyBundle
 
 class MyApplicationService {
 
